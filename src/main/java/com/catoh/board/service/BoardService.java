702eef5428652427f500db45510bfa76a72682a4
Caroline Kim <caroline.kim@g.austincc.edu>
2020-02-20 15:38:25 -0600

    ver9. create getPost and deletePost to edit and erase the existing post

--- src/main/java/com/catoh/board/service/BoardService.java
+++ src/main/java/com/catoh/board/service/BoardService.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Service;
 import javax.transaction.Transactional;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 
 @AllArgsConstructor // constructor declaration for insert to board repository
@@ -40,5 +41,27 @@ public class BoardService {
     public Long savePost(BoardDto boardDto) {
         // save(): in charge of insert and update to db
         return boardRepository.save(boardDto.toEntity()).getId();
+    }
+
+    // Creating entities for boardDTO. This is will transfer the existing posting to be edit or erase
+    @Transactional
+    public BoardDto getPost(Long id) {
+        Optional<BoardEntity> boardEntityWrapper = boardRepository.findById(id);
+        BoardEntity boardEntity = boardEntityWrapper.get();
+
+        BoardDto boardDTO = BoardDto.builder()
+                .id(boardEntity.getId())
+                .title(boardEntity.getTitle())
+                .content(boardEntity.getContent())
+                .writer(boardEntity.getWriter())
+                .createdDate(boardEntity.getCreatedDate())
+                .build();
+
+        return boardDTO;
+    }
+
+    @Transactional
+    public void deletePost(Long id) {
+        boardRepository.deleteById(id);
     }
 }
